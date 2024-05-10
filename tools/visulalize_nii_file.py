ff949480ed8a88f6542ee0635c11907555f1f2b9
Ofir Gilad <ofir.gilad9631@gmail.com>
2024-05-11 00:58:03 +0300

    created the dataset

--- tools/visulalize_nii_file.py
+++ tools/visulalize_nii_file.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import nibabel as nib
+import cv2
+import random
+import os
 
 
 def convert_nii_to_numpy(data_file):
@@ -79,8 +82,8 @@ def test_project_3d_to_2d():
     save_images(front_image, up_image, left_image, output_idx)
 
 
-def export_images():
-    import os
+# Src Dataset
+def create_dataset_src_images():
     folder_path = "../skel_np"
     data_filepaths = os.listdir(folder_path)
 
@@ -102,10 +105,47 @@ def export_images():
         )
 
 
+# Dst Dataset
+def randomly_remove_white_points(image, num_points=10):
+    # Find the white pixels
+    white_pixels = np.where(image == 255)
+
+    # Randomly select 'num_points' indices
+    selected_indices = random.sample(range(len(white_pixels[0])), num_points)
+
+    # Change the selected white pixels to black (0)
+    for idx in selected_indices:
+        x, y = white_pixels[0][idx], white_pixels[1][idx]
+        image[x, y] = 0
+
+    return image
+
+
+def create_dataset_dst_images():
+    src_folder = "./cropped_src_images"
+    dst_folder = "./cropped_dst_images"
+
+    image_filepaths = os.listdir(src_folder)
+    for image_filepath in image_filepaths:
+        image_filepath = os.path.join(src_folder, image_filepath)
+        image = cv2.imread(image_filepath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        try:
+            image = randomly_remove_white_points(image, num_points=150)
+        except:  # If the image has less than 150 white points
+            image = randomly_remove_white_points(image, num_points=50)
+
+        image_output_filepath = image_filepath.replace(src_folder, dst_folder)
+        plt.imsave(image_output_filepath, image, cmap="gray")
+
+
 def main():
     # test_plot_3d_data()
     # test_project_3d_to_2d()
-    export_images()
+
+    # create_dataset_src_images()
+    create_dataset_dst_images()
 
 
 if __name__ == "__main__":
