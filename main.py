2a772c06a56b0f108e6f1b82291c8c60bd71b77d
Ofir Gilad <85561260+OfirGilad369@users.noreply.github.com>
2024-05-25 12:52:25 +0300

    fixed output

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import argparse
 import os
-import numpy as np
-import imageio
-from scipy import ndimage
 import torch
-from torchvision.utils import save_image
 
 from model import Network
 from train import Trainer
-from utils import get_interpolations
 import matplotlib.pyplot as plt
 
 
@@ -27,6 +22,7 @@ def predict_model(model):
     trainer = Trainer(args=args, model=model)
 
     with torch.no_grad():
+        # Get the images from the test loader
         input_images, _ = next(iter(trainer.test_input_loader))
         target_images, _ = next(iter(trainer.test_target_loader))
         input_images = input_images.to(trainer.device)
@@ -37,55 +33,47 @@ def predict_model(model):
         if target_images.dtype != torch.float32:
             target_images = target_images.float()
 
-        model.eval()
-        num_examples = 64
-
+        # Create holes in the input images
         trainer.create_holes(input_images)
-        generated_samples = model(input_images)
-        generated_samples = generated_samples.cpu().detach()
 
-        for i in range(16):
-            ax = plt.subplot(4, 4, i + 1)
+        model.eval()
+        output_images = model(input_images)
+
+        # Detach the images from the cuda and move them to CPU
+        if trainer.args.cuda:
+            input_images = input_images.cpu().detach()
+            target_images = target_images.cpu().detach()
+            output_images = output_images.cpu().detach()
+
+        # Create a grid of images
+        columns = 3
+        rows = 16
+        fig = plt.figure(figsize=(columns + 0.5, rows + 0.5))
+        ax = []
+        for i in range(rows):
+            # Input
+            ax.append(fig.add_subplot(rows, columns, i * 3 + 1))
             plt.imshow(input_images[i].reshape(28, 28), cmap="gray")
-            # plt.imshow(target_images[i].reshape(28, 28), cmap="gray")
-            # plt.imshow(generated_samples[i].reshape(28, 28), cmap="gray")
-            plt.xticks([])
-            plt.yticks([])
 
-        plt.show()
-        # images_per_row = 16
-        # interpolations = get_interpolations(args, model, trainer.device, images, images_per_row)
+            # Target
+            ax.append(fig.add_subplot(rows, columns, i * 3 + 2))
+            plt.imshow(target_images[i].reshape(28, 28), cmap="gray")
 
-        # sample = torch.randn(64, args.embedding_size).to(trainer.device)
-        # sample = model.decode(sample).cpu()
-        #
-        # if args.dataset != 'Trees':
-        #     save_image(sample.view(64, 1, 28, 28),
-        #                '{}/sample_{}.png'.format(args.results_path, args.dataset))
-        #     save_image(interpolations.view(-1, 1, 28, 28),
-        #                '{}/interpolations_{}.png'.format(args.results_path, args.dataset), nrow=images_per_row)
-        #     interpolations = interpolations.cpu()
-        #     interpolations = np.reshape(interpolations.data.numpy(), (-1, 28, 28))
-        #     interpolations = ndimage.zoom(interpolations, 5, order=1)
-        #     interpolations *= 256
-        #     imageio.mimsave('{}/animation_{}.gif'.format(args.results_path, args.dataset), interpolations.astype(np.uint8))
-        # else:
-        #     save_image(sample.view(64, 1, 64, 64),
-        #                '{}/sample_{}.png'.format(args.results_path, args.dataset))
-        #     save_image(interpolations.view(-1, 1, 64, 64),
-        #                '{}/interpolations_{}.png'.format(args.results_path, args.dataset), nrow=images_per_row)
-        #     interpolations = interpolations.cpu()
-        #     interpolations = np.reshape(interpolations.data.numpy(), (-1, 64, 64))
-        #     interpolations = ndimage.zoom(interpolations, 5, order=1)
-        #     interpolations *= 256
-        #     imageio.mimsave('{}/animation_{}.gif'.format(args.results_path, args.dataset),
-        #                     interpolations.astype(np.uint8))
+            # Output
+            ax.append(fig.add_subplot(rows, columns, i * 3 + 3))
+            plt.imshow(output_images[i].reshape(28, 28), cmap="gray")
+
+        ax[0].set_title("Input:")
+        ax[1].set_title("Target:")
+        ax[2].set_title("Output:")
+        fig.tight_layout()
+        plt.savefig(os.path.join(args.results_path, f'output_{args.dataset}.png'))
 
 
 def main():
     model = Network(args)
 
-    # train_model(model=model)
+    train_model(model=model)
     predict_model(model=model)
 
 
