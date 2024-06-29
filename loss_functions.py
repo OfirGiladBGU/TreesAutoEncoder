fa12dd1e426ecc23ffd1ffbffc5e996a4cca0942
Ofir Gilad <ofir.gilad9631@gmail.com>
2024-06-29 22:42:13 +0300

    added more loss functions

--- loss_functions.py
+++ loss_functions.py
@@ -4,20 +4,40 @@ import torch.nn as nn
 
 import pytorch_ssim
 import vgg_loss
+from torchmetrics.segmentation import MeanIoU
+from torcheval.metrics.functional import multiclass_f1_score
 
 
-def reconstruction_loss(out, target):
+#####################
+# Utility functions #
+#####################
+def reshape_data(data, input_size):
+    return data.view(-1, input_size[0], input_size[1], input_size[2])
+
+
+def reshape_inputs(input_images, target_images, input_size):
+    input_images = reshape_data(input_images, input_size)
+    target_images = reshape_data(target_images, input_size)
+    return input_images, target_images
+
+
+###################
+# GPT Suggestions #
+###################
+def reconstruction_loss(out, target, input_size=(1, 64, 64)):
+    reshaped_out, reshaped_target = reshape_inputs(out, target, input_size)
+
     mse_loss = torch.nn.MSELoss()
-    return mse_loss(out, target)
+    return mse_loss(reshaped_out, reshaped_target)
 
 
 # From VGG Loss
-def perceptual_loss(out, target, device):
+def perceptual_loss(out, target, device, input_size=(1, 64, 64)):
+    reshaped_out, reshaped_target = reshape_inputs(out, target, input_size)
+
     # Change shapes to 3 channels
-    resized_out_x = out.view(-1, 1, 64, 64)
-    rgb_out = resized_out_x.repeat(1, 3, 1, 1)
-    resized_target = target.view(-1, 1, 64, 64)
-    rgb_target = resized_target.repeat(1, 3, 1, 1)
+    rgb_out = reshaped_out.repeat(1, 3, 1, 1)
+    rgb_target = reshaped_target.repeat(1, 3, 1, 1)
 
     crit = vgg_loss.WeightedLoss(
         losses=[
@@ -32,7 +52,9 @@ def perceptual_loss(out, target, device):
     return loss
 
 
-def edge_loss(out, target, device):
+def edge_loss(out, target, device, input_size=(1, 64, 64)):
+    reshaped_out, reshaped_target = reshape_inputs(out, target, input_size)
+
     # Sobel filters
     x_filter = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
     y_filter = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
@@ -49,16 +71,16 @@ def edge_loss(out, target, device):
     convx.weight = nn.Parameter(weights_x)
     convy.weight = nn.Parameter(weights_y)
 
-    g1_x = convx(out)
-    g2_x = convx(target)
-    g1_y = convy(out)
-    g2_y = convy(target)
+    g1_x = convx(reshaped_out)
+    g2_x = convx(reshaped_target)
+    g1_y = convy(reshaped_out)
+    g2_y = convy(reshaped_target)
 
     # Option 2
-    # g1_x = nn.functional.conv2d(input=out, weight=weights_x, stride=1, padding=1)
-    # g2_x = nn.functional.conv2d(input=target, weight=weights_x, stride=1, padding=1)
-    # g1_y = nn.functional.conv2d(input=out, weight=weights_y, stride=1, padding=1)
-    # g2_y = nn.functional.conv2d(input=target, weight=weights_y, stride=1, padding=1)
+    # g1_x = nn.functional.conv2d(input=reshaped_out, weight=weights_x, stride=1, padding=1)
+    # g2_x = nn.functional.conv2d(input=reshaped_target, weight=weights_x, stride=1, padding=1)
+    # g1_y = nn.functional.conv2d(input=reshaped_out, weight=weights_y, stride=1, padding=1)
+    # g2_y = nn.functional.conv2d(input=reshaped_target, weight=weights_y, stride=1, padding=1)
 
     # Calculate the gradient magnitude
     g_1 = torch.sqrt(torch.pow(g1_x, 2) + torch.pow(g1_y, 2))
@@ -67,14 +89,42 @@ def edge_loss(out, target, device):
     return torch.mean((g_1 - g_2).pow(2))
 
 
-# From VGG Loss
-def total_variation_lost(out, target, device):
+###############################
+# Other useful loss functions #
+###############################
+def mean_iou(out, target, input_size=(1, 64, 64)):
+    reshaped_out, reshaped_target = reshape_inputs(out, target, input_size)
+
+    scaled_out = reshaped_out * 255
+    scaled_out = scaled_out.int()
+
+    scaled_target = reshaped_target * 255
+    scaled_target = scaled_target.int()
+
+    miou = MeanIoU(num_classes=255)
+    return miou(scaled_out, scaled_target)
+
+
+def total_variation_lost(out, target, device, input_size=(1, 64, 64)):
+    reshaped_out, reshaped_target = reshape_inputs(out, target, input_size)
+
     tv_loss = vgg_loss.TVLoss(p=2).to(device)
-    return tv_loss(out, target)
+    return tv_loss(reshaped_out, reshaped_target)
 
 
 def ssim_loss(out, target, input_size=(1, 64, 64)):
-    return pytorch_ssim.ssim(
-        out.view(-1, input_size[0], input_size[1], input_size[2]),
-        target.view(-1, input_size[0], input_size[1], input_size[2])
-    )
+    reshaped_out, reshaped_target = reshape_inputs(out, target, input_size)
+
+    return pytorch_ssim.ssim(reshaped_out, reshaped_target)
+
+
+def f1_loss(out, target, input_size=(1, 64, 64)):
+    reshaped_out, reshaped_target = reshape_inputs(out, target, input_size)
+
+    scaled_out = reshaped_out * 255
+    scaled_out = scaled_out.int()
+
+    scaled_target = reshaped_target * 255
+    scaled_target = scaled_target.int()
+
+    return multiclass_f1_score(scaled_out, scaled_target, num_classes=255)
