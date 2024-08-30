48dad2ab6404232c2af4e4f1e122e9e771dd1d7d
OfirGilad <ofirgila@post.bgu.ac.il>
2024-08-30 19:17:35 +0300

    small design fix

--- datasets.py
+++ datasets.py
@@ -1,6 +1,7 @@
 import torch
 from torchvision import datasets, transforms
 import os
+from torch.utils.data import DataLoader
 
 from tools.dataset_and_dataloaders import TreesCustomDataloader, TreesCustomDataloader3D
 
@@ -9,44 +10,68 @@ class MNIST(object):
     def __init__(self, args):
         kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
         self.train_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('data/mnist', train=True, download=True, transform=transforms.ToTensor()),
-            batch_size=args.batch_size, shuffle=False, **kwargs)
+            dataset=datasets.MNIST(root='data/mnist', train=True, download=True, transform=transforms.ToTensor()),
+            batch_size=args.batch_size,
+            shuffle=False,
+            **kwargs
+        )
         self.test_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('data/mnist', train=False, transform=transforms.ToTensor()),
-            batch_size=args.batch_size, shuffle=False, **kwargs)
+            datasets.MNIST(root='data/mnist', train=False, transform=transforms.ToTensor()),
+            batch_size=args.batch_size,
+            shuffle=False,
+            **kwargs
+        )
 
 
 class EMNIST(object):
     def __init__(self, args):
         kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
         self.train_loader = torch.utils.data.DataLoader(
-            datasets.EMNIST('data/emnist', train=True, download=True, split='byclass', transform=transforms.ToTensor()),
-            batch_size=args.batch_size, shuffle=False, **kwargs)
+            datasets.EMNIST(root='data/emnist', train=True, download=True, split='byclass', transform=transforms.ToTensor()),
+            batch_size=args.batch_size,
+            shuffle=False,
+            **kwargs
+        )
         self.test_loader = torch.utils.data.DataLoader(
-            datasets.EMNIST('data/emnist', train=False, split='byclass', transform=transforms.ToTensor()),
-            batch_size=args.batch_size, shuffle=False, **kwargs)
+            datasets.EMNIST(root='data/emnist', train=False, split='byclass', transform=transforms.ToTensor()),
+            batch_size=args.batch_size,
+            shuffle=False,
+            **kwargs
+        )
 
 
 class FashionMNIST(object):
     def __init__(self, args):
         kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
         self.train_loader = torch.utils.data.DataLoader(
-            datasets.FashionMNIST('data/fmnist', train=True, download=True, transform=transforms.ToTensor()),
-            batch_size=args.batch_size, shuffle=False, **kwargs)
+            datasets.FashionMNIST(root='data/fmnist', train=True, download=True, transform=transforms.ToTensor()),
+            batch_size=args.batch_size,
+            shuffle=False,
+            **kwargs
+        )
         self.test_loader = torch.utils.data.DataLoader(
-            datasets.FashionMNIST('data/fmnist', train=False, transform=transforms.ToTensor()),
-            batch_size=args.batch_size, shuffle=False, **kwargs)
+            datasets.FashionMNIST(root='data/fmnist', train=False, transform=transforms.ToTensor()),
+            batch_size=args.batch_size,
+            shuffle=False,
+            **kwargs
+        )
 
 
 class CIFAR10(object):
     def __init__(self, args):
-        tf = transforms.ToTensor()
-        train_set = datasets.CIFAR10('data/cifar10', download=True, transform=tf)
-        self.train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True,
-                                                        num_workers=2, pin_memory=True)
-        test_set = datasets.CIFAR10('data/cifar10', train=False, transform=tf)
-        self.test_loader = torch.utils.data.DataLoader(test_set, batch_size=args.batch_size,
-                                                       num_workers=2, pin_memory=True)
+        kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+        self.train_loader = torch.utils.data.DataLoader(
+            datasets.CIFAR10(root='data/cifar10', download=True, transform=transforms.ToTensor()),
+            batch_size=args.batch_size,
+            shuffle=False,
+            **kwargs
+        )
+        self.test_loader = torch.utils.data.DataLoader(
+            datasets.CIFAR10(root='data/cifar10', train=False, transform=transforms.ToTensor()),
+            batch_size=args.batch_size,
+            shuffle=False,
+            **kwargs
+        )
 
 
 # Custom Dataset
@@ -80,8 +105,8 @@ class TreesDatasetV1(object):
 class TreesDatasetV2(object):
     def __init__(self, args):
         src_path = os.path.join(str(os.path.dirname(__file__)), "tools", "data", "mini_cropped_images")
-        data_paths = [src_path]
 
+        data_paths = [src_path]
         trees_dataloader = TreesCustomDataloader(data_paths=data_paths, args=args)
         self.train_loader, self.test_loader = trees_dataloader.get_dataloader()
 
@@ -94,6 +119,7 @@ class TreesDataset3DV1(object):
         data_paths = [src_path, dst_path]
         trees_dataloader = TreesCustomDataloader3D(data_paths=data_paths, args=args)
         self.train_loader, self.test_loader = trees_dataloader.get_dataloader()
+
 
 class TreesDataset3DV2(object):
     def __init__(self, args):
