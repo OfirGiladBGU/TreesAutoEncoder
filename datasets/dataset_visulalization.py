f83436418fd3cd995e66684f19b08b5d9e6faf25
OfirGilad <ofirgila@post.bgu.ac.il>
2024-09-18 13:12:20 +0300

    added 2d visualizations

--- datasets/dataset_visulalization.py
+++ datasets/dataset_visulalization.py
@@ -2,11 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import itertools
+import cv2
 
 from datasets.dataset_utils import convert_nii_gz_to_numpy
 from datasets.dataset_list import CROPPED_PATH, VISUALIZATION_RESULTS_PATH, RESULTS_PATH, PREDICT_PIPELINE_RESULTS_PATH
 
 
+####################
+# 3D visualization #
+####################
 def matplotlib_plot_3d(data_3d: np.ndarray, save_filename):
     print(f"Save filename: '{save_filename}*'\nData shape: '{data_3d.shape}'\n")
     data_3d = data_3d
@@ -95,10 +99,60 @@ def full_plot_3d(include_pipeline_results=False):
             matplotlib_plot_3d(data_3d=numpy_3d_data, save_filename=save_filename)
 
 
+####################
+# 2D visualization #
+####################
+IMAGES_6_VIEWS = ['top', 'bottom', 'front', 'back', 'left', 'right']
+
+
+def matplotlib_plot_2d(save_filepath, data_2d_list):
+    columns = 6
+    rows = 1
+    fig = plt.figure(figsize=(columns + 0.5, rows + 0.5))
+    ax = list()
+
+    # 2D Input
+    for j in range(columns):
+        ax.append(fig.add_subplot(rows, columns, 0 * columns + j + 1))
+        numpy_image = data_2d_list[j]
+        numpy_image = numpy_image
+        plt.imshow(numpy_image, cmap='gray')
+        ax[j].set_title(f"View {IMAGES_6_VIEWS[j]}:")
+
+    fig.tight_layout()
+    plt.savefig(save_filepath)
+    plt.close(fig)
+
+
+def full_plot_2d():
+    data_3d_basename = "PA000005_11899"
+
+    folder_paths = {
+        "labels_2d": os.path.join(CROPPED_PATH, "labels_2d_v6"),
+        "preds_2d": os.path.join(CROPPED_PATH, "preds_2d_v6"),
+        "preds_fixed_2d": os.path.join(CROPPED_PATH, "preds_fixed_2d_v6")
+    }
+
+    for key, value in folder_paths.items():
+        format_of_2d_images = os.path.join(value, f"{data_3d_basename}_<VIEW>.png")
+        # Projections 2D
+        data_2d_list = list()
+        for image_view in IMAGES_6_VIEWS:
+            image_path = format_of_2d_images.replace("<VIEW>", image_view)
+            image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+            data_2d_list.append(image)
+
+        save_path = os.path.join(VISUALIZATION_RESULTS_PATH, key)
+        os.makedirs(name=save_path, exist_ok=True)
+        save_filepath = os.path.join(str(save_path), f"{data_3d_basename}")
+        matplotlib_plot_2d(save_filepath=save_filepath, data_2d_list=data_2d_list)
+
+
 def main():
     # single_plot_3d()
     # full_plot_3d()
-    full_plot_3d(include_pipeline_results=True)
+    # full_plot_3d(include_pipeline_results=True)
+    full_plot_2d()
 
 
 if __name__ == "__main__":
