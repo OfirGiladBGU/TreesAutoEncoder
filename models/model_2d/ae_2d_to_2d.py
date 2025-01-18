c3f661041cccd96fdfb9bbbe8fc1ccbbe1a8992b
OfirGilad <ofirgila@post.bgu.ac.il>
2025-01-18 02:03:34 +0200

    model started building connectivity

--- models/model_2d/ae_2d_to_2d.py
+++ models/model_2d/ae_2d_to_2d.py
@@ -276,9 +276,9 @@ class Network2D(nn.Module):
 
         self.model_name = 'ae_2d_to_2d'
         self.input_size = args.input_size
-        self.additional_tasks = [
-            "confidence map"
-        ]
+        # self.additional_tasks = [
+        #     "confidence map"
+        # ]
 
         # Encoder with larger kernels
         self.encoder1 = nn.Sequential(
@@ -325,6 +325,10 @@ class Network2D(nn.Module):
             nn.Sigmoid()  # Confidence map, values between 0 and 1
         )
 
+        # # Skip connection layers to align encoder features with decoder inputs
+        # self.skip_conv1 = nn.Conv2d(64, 64, kernel_size=1)
+        # self.skip_conv2 = nn.Conv2d(128, 128, kernel_size=1)
+
     def forward(self, x):
         # Encoding
         x1 = self.encoder1(x)
@@ -339,9 +343,85 @@ class Network2D(nn.Module):
         recon = self.decoder2(recon + x2)  # Skip connection
         recon = self.decoder3(recon + x1)  # Skip connection
 
-        # Decoding for confidence map
-        confidence = self.confidence_decoder1(x3)
-        confidence = self.confidence_decoder2(confidence + x2)  # Skip connection
-        confidence = self.confidence_decoder3(confidence + x1)  # Skip connection
+        # # Decoding for confidence map
+        # confidence = self.confidence_decoder1(x3)
+        # confidence = self.confidence_decoder2(confidence + x2)  # Skip connection
+        # confidence = self.confidence_decoder3(confidence + x1)  # Skip connection
 
-        return recon, confidence
+        # return recon, confidence
+
+        # # Decoder with skip connections
+        # d1 = self.decoder1(x3)  # Output: (B, 128, H/4, W/4)
+        # d1 = d1 + self.skip_conv2(x2)  # Skip connection from encoder2
+        #
+        # d2 = self.decoder2(d1)  # Output: (B, 64, H/2, W/2)
+        # d2 = d2 + self.skip_conv1(x1)  # Skip connection from encoder1
+        #
+        # recon = self.decoder3(d2)  # Output: (B, 1, H, W)
+        return recon
+
+# ##########
+# # Test 5 #
+# ##########
+#
+# import torch.nn.functional as F
+#
+# class Network2D(nn.Module):
+#     def __init__(self, args: argparse.Namespace):
+#         super(Network2D, self).__init__()
+#
+#         self.model_name = 'ae_2d_to_2d'
+#         self.input_size = args.input_size
+#
+#         # Encoder
+#         self.enc1 = self.conv_block(1, 64)
+#         self.enc2 = self.conv_block(64, 128)
+#         self.enc3 = self.conv_block(128, 256)
+#
+#         # Bottleneck
+#         self.bottleneck = self.conv_block(256, 512)
+#
+#         # Decoder
+#         self.dec3 = self.conv_block(512 + 256, 256)
+#         self.dec2 = self.conv_block(256 + 128, 128)
+#         self.dec1 = self.conv_block(128 + 64, 64)
+#
+#         # Final output
+#         self.final = nn.Conv2d(64, 1, kernel_size=1)
+#
+#     def conv_block(self, in_channels, out_channels):
+#         return nn.Sequential(
+#             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+#             nn.ReLU(inplace=True),
+#             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+#             nn.ReLU(inplace=True)
+#         )
+#
+#     def forward(self, x):
+#         # Encoder
+#         c1 = self.enc1(x)
+#         p1 = F.max_pool2d(c1, kernel_size=2)
+#         c2 = self.enc2(p1)
+#         p2 = F.max_pool2d(c2, kernel_size=2)
+#         c3 = self.enc3(p2)
+#         p3 = F.max_pool2d(c3, kernel_size=2)
+#
+#         # Bottleneck
+#         bn = self.bottleneck(p3)
+#
+#         # Decoder
+#         u3 = F.interpolate(bn, scale_factor=2, mode='bilinear', align_corners=True)
+#         u3 = torch.cat([u3, c3], dim=1)
+#         d3 = self.dec3(u3)
+#
+#         u2 = F.interpolate(d3, scale_factor=2, mode='bilinear', align_corners=True)
+#         u2 = torch.cat([u2, c2], dim=1)
+#         d2 = self.dec2(u2)
+#
+#         u1 = F.interpolate(d2, scale_factor=2, mode='bilinear', align_corners=True)
+#         u1 = torch.cat([u1, c1], dim=1)
+#         d1 = self.dec1(u1)
+#
+#         # Final output
+#         output = self.final(d1)
+#         return torch.sigmoid(output)
